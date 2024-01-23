151a8b785c422feda11bd8c416a2db2ef42935f9
IanCist <iantcisternas@gmail.com>
2024-01-23 14:56:40 -0500

    added more java script

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 //Function to Calc and display Tip, Total Bill and amount per person
 
 function calculateAndDisplay(){
-    
+    //: Get the values from the HTML form
+    let bill = parseFloat(document.getElementById('totalBill').value)
+    let numOfPpl = parseInt(document.getElementById('numOfPeople').value)
+    let serviceQuality = document.getElementById('serviceQuality').value
+
+    //calculate tip, total bill, and the Amount each person owes
+    let tip = calculateTip(bill, serviceQuality)
+    let totalBill = calculateTotalBill
+    let amtPerPers = calcAmtPerPers(totalBill, numOfPpl)
+
+    //Display the Results in the HTMl
+    document.getElementById('tipResut').innerText = 'Tip $' + tip.toFixed(2)
+    document.getElementById('totalBillResult').innerText = 
+    'totalBill: $' + totalBill.toFixed(2)
+    document.getElementById('amtPerPersonResult').innerText =
+    'Amount per person: $' + amtPerPers.toFixed(2)
 }
